c85e752a0e4b14078198e5ba12cd94b5d6019e41
farangiz0509 <farangiztolibova75@gmail.com>
2026-01-17 10:04:30 +0545

    done

--- app/schemas.py
+++ app/schemas.py
@@ -20,3 +20,14 @@ class CategoryCreate(BaseModel):
 class CategoryUpdate(BaseModel):
     name: str | None = Field(max_length=100)
     description: str | None = None
+
+
+class ProductResponse(BaseModel):
+    product_id: int
+    name: str = Field(max_length=64)
+    price: float
+    in_stock: bool
+    category_id: int
+
+    class Config:
+        from_attributes = True
